de8e711282a8cb68956447b232c05a94fb8d345d
zsondroid <jiline1117@naver.com>
2023-12-13 10:45:25 +0900

    [fix] 하나의 ViewModel 바라보도록 수정

--- app/src/main/java/com/zsondroid/hiltapplication/MainActivity.kt
+++ app/src/main/java/com/zsondroid/hiltapplication/MainActivity.kt
@@ -5,15 +5,13 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.zsondroid.hiltapplication.databinding.ActivityMainBinding
 import com.zsondroid.hiltapplication.room.User
-import com.zsondroid.hiltapplication.viewModel.RetrofitViewModel
-import com.zsondroid.hiltapplication.viewModel.RoomViewModel
+import com.zsondroid.hiltapplication.viewModel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val retrofitViewModel: RetrofitViewModel by viewModels()
-    private val roomViewModel: RoomViewModel by viewModels()
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,14 +20,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.lifecycleOwner = this
-        binding.retrofitViewModel = retrofitViewModel
-        binding.roomViewModel = roomViewModel
+        binding.mainViewModel = mainViewModel
 
         addTestUserData()
     }
 
     private fun addTestUserData() {
         val newUser = User("지손", "27", "010-1234-1234")
-        roomViewModel.insert(newUser)
+        mainViewModel.insert(newUser)
     }
 }
--- app/src/main/java/com/zsondroid/hiltapplication/viewModel/MainViewModel.kt
+++ app/src/main/java/com/zsondroid/hiltapplication/viewModel/MainViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.zsondroid.hiltapplication.data.Emoji
+import com.zsondroid.hiltapplication.model.RetrofitRepository
 import com.zsondroid.hiltapplication.model.RoomRepository
 import com.zsondroid.hiltapplication.room.User
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,14 +15,28 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class RoomViewModel @Inject constructor(private val repository: RoomRepository) : ViewModel() {
+class MainViewModel @Inject constructor(private val retrofitRepository: RetrofitRepository, private val roomRepository: RoomRepository) : ViewModel() {
+    private val _emojiData = MutableLiveData<Emoji>()
+    val emojiData: LiveData<Emoji> = _emojiData
 
     private val _userData = MutableLiveData<List<User>>()
     val userData: LiveData<List<User>> = _userData
 
+    fun setRandomEmojiData() {
+        var resultEmojiData: Emoji
+
+        viewModelScope.launch(Dispatchers.IO) {
+            resultEmojiData = retrofitRepository.getRandomEmojiData()
+
+            withContext(Dispatchers.Main) {
+                _emojiData.value = resultEmojiData
+            }
+        }
+    }
+
     fun getAll() {
         viewModelScope.launch(Dispatchers.IO) {
-            val resultUserData = repository.getAll()
+            val resultUserData = roomRepository.getAll()
 
             withContext(Dispatchers.Main) {
                 _userData.value = resultUserData
@@ -30,7 +46,7 @@ class RoomViewModel @Inject constructor(private val repository: RoomRepository) 
 
     fun insert(user: User) {
         viewModelScope.launch(Dispatchers.IO) {
-            repository.insert(user)
+            roomRepository.insert(user)
         }
     }
 }
